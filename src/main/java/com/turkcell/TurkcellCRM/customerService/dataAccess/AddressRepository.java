6c573177fa7e87861a543a8cf4fac97ad9cc91cd
Elnur <akkurtelnur11@gmail.com>
2024-04-26 19:26:07 +0300

    initial commit

---
+++ src/main/java/com/turkcell/TurkcellCRM/customerService/dataAccess/AddressRepository.java
@@ -0,0 +1,8 @@
+package com.turkcell.TurkcellCRM.customerService.dataAccess;
+
+import com.turkcell.TurkcellCRM.customerService.entities.concretes.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository  extends JpaRepository<Address,Integer> {
+
+}
